6d5bea3aa6521e7649eb9741aa8c7dd1c7bac82e
정윤성 <136976803+JYS1025@users.noreply.github.com>
2024-07-24 13:51:33 +0000

    Implement sentence-tokenize scale NLPP

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import nltk
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from collections import Counter
 
@@ -8,23 +8,45 @@ from preprocess import clean_by_freq
 from preprocess import clean_by_len
 from preprocess import clean_by_stopwords
 from preprocess import stemming_by_porter
+from preprocess import combine
+from preprocess import pos_tagger
+from preprocess import words_lemmatizer
 
+# NLTK 리소스 다운로드
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
 
-df = pd.read_csv('imdb.tsv', delimiter='\\t')
+# 데이터프레임 읽기
+df = pd.read_csv('imdb.tsv', delimiter='\\t', engine='python')
 
-#Normalize
+# Normalize
 df['review'] = df['review'].str.lower()
 
-#Tokenize
+# Tokenize
 df['word_tokens'] = df['review'].apply(word_tokenize)
+df['sent_tokens'] = df['review'].apply(sent_tokenize)
+
+# pos-tagging
+df['pos_tagged_tokens'] = df['sent_tokens'].apply(pos_tagger)
+
+# 표제어 추출
+df['lemmatized_tokens'] = df['pos_tagged_tokens'].apply(words_lemmatizer)
 
 stopwords_set = set(stopwords.words('english'))
 
-df['cleaned_tokens'] = df['word_tokens'].apply(lambda x: clean_by_freq(x, 1))
+# 클린 작업
+df['cleaned_tokens'] = df['lemmatized_tokens'].apply(lambda x: clean_by_freq(x, 1))
 df['cleaned_tokens'] = df['cleaned_tokens'].apply(lambda x: clean_by_len(x, 2))
 df['cleaned_tokens'] = df['cleaned_tokens'].apply(lambda x: clean_by_stopwords(x, stopwords_set))
 
 # 어간 추출
 df['stemmed_tokens'] = df['cleaned_tokens'].apply(stemming_by_porter)
+
+# 문자열 통합
+df['combined_corpus'] = df['stemmed_tokens'].apply(combine)
+
+# Interger Encoding
+
+
--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,19 @@
 from collections import Counter
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk import word_tokenize
+from nltk.tag import pos_tag
+from nltk.corpus import wordnet as wn
+
+lemmatizer = WordNetLemmatizer()
 
 def clean_by_freq(tokenized_words, cut_off_count):
     vocab = Counter(tokenized_words)
-
-    uncommon_words = {key for key, value in vocab.item if value <= cut_off_count}
+    uncommon_words = {key for key, value in vocab.items() if value <= cut_off_count}
     cleaned_words = [word for word in tokenized_words if word not in uncommon_words]
-
     return cleaned_words
 
 def clean_by_len(tokenized_words, cut_off_len):
-    cleaned_words = []
-    for word in tokenized_words:
-        if len(word) > cut_off_len:
-            cleaned_words.append(word)
-
+    cleaned_words = [word for word in tokenized_words if len(word) > cut_off_len]
     return cleaned_words
 
 def clean_by_stopwords(tokenized_words, stopwords_set):
@@ -22,10 +21,37 @@ def clean_by_stopwords(tokenized_words, stopwords_set):
 
 def stemming_by_porter(tokenized_words):
     porter_stemmer = PorterStemmer()
-    porter_stemmed_words = []
+    return [porter_stemmer.stem(word) for word in tokenized_words]
 
-    for word in tokenized_words:
-        stem = porter_stemmer.stem(word)
-        porter_stemmed_words.append(stem)
-        
-    return porter_stemmed_words
+def combine(sentence):
+    return ' '.join(sentence)
+
+def pos_tagger(tokenized_sent):
+    pos_tagged_words = []
+    for sentence in tokenized_sent:
+        words = word_tokenize(sentence)
+        pos_tagged = pos_tag(words)
+        pos_tagged_words.extend(pos_tagged)
+    return pos_tagged_words
+
+def penn_to_wn(tag):
+    if tag.startswith('N'):
+        return wn.NOUN
+    elif tag.startswith('V'):
+        return wn.VERB
+    elif tag.startswith('J'):
+        return wn.ADJ
+    elif tag.startswith('R'):
+        return wn.ADV
+    else:
+        return None
+
+def words_lemmatizer(pos_tagged_words):
+    lemmatized_words = []
+    for word, tag in pos_tagged_words:
+        wn_tag = penn_to_wn(tag)
+        if wn_tag in (wn.NOUN, wn.ADJ, wn.ADV, wn.VERB):
+            lemmatized_words.append(lemmatizer.lemmatize(word, wn_tag))
+        else:
+            lemmatized_words.append(word)
+    return lemmatized_words
